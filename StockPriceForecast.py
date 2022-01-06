26c670a9120a8e1441ada68ef8899d65a43a4cd5
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2022-01-06 14:55:20 +0300

    Add files via upload

---
+++ StockPriceForecast.py
@@ -0,0 +1,143 @@
+import numpy as np
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+import pandas_datareader.data as web
+import math
+
+from numpy import array
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import LSTM
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import accuracy_score
+
+#Load Data
+company = 'XU100.IS'
+
+start = dt.datetime(2016, 1, 1)
+end = dt.datetime(2020,7,24)      #(year, month, date)
+
+data = web.DataReader(company, 'yahoo', start-dt.timedelta(days=365), end)
+
+#Prepare Data
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+prediction_days = 60
+
+x_train = []
+y_train = []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x,0])
+    y_train.append(scaled_data[x,0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+#Build the Model
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1],1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1, activation="relu"))
+
+print(model.summary())
+
+model.compile(optimizer='adam', loss='mse')
+model.fit(x_train, y_train, epochs=14, batch_size=64)
+
+'''Test the Model  Accuracy on Existing Data'''
+
+#Load Test Data
+test_start = dt.datetime(2019,7,24)
+test_end = dt.datetime(2020,7,24)
+tomorrow = test_end + dt.timedelta(days=1)
+
+test_data = web.DataReader(company, 'yahoo', test_start, test_end)
+actual_prices = test_data['Close'].values
+
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis = 0 )
+
+model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days: ].values
+model_inputs = model_inputs.reshape(-1,1)
+model_inputs = scaler.transform(model_inputs)
+
+#Make Prediction on Test Data
+x_test = []
+y_test = []
+
+for x in range(prediction_days, len(model_inputs)):
+    x_test.append(model_inputs[x - prediction_days: x, 0])
+    y_test.append(model_inputs[x,0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+y_test = np.array(y_test)
+
+predicted_prices = model.predict(x_test)
+predicted_prices = scaler.inverse_transform(predicted_prices)  
+
+#Plot the Test Predictions
+plt.figure(figsize=(12,5), dpi=100)
+plt.plot(actual_prices, color = 'black', label = f"Actual {company} Price")
+plt.plot(predicted_prices, color = 'red', label = f"Predicted {company} Price")
+plt.title(f"{company} Share Price")
+plt.xlabel('Time')
+plt.ylabel(f"{company} Share Price")
+plt.legend()
+plt.show()
+
+# Forecasting Next Day
+
+real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs+1), 0]]
+real_data = np.array(real_data)
+real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1],1))
+
+prediction = model.predict(real_data)
+prediction = scaler.inverse_transform(prediction)
+print(f"Date: {tomorrow}: {company} CLOSE Price Forecasting: {prediction}") # Stock
+
+# MAPE(Mean Absolute Percentage Error) Expected Forecasting Accuracy SCORE
+mape = np.mean(np.abs(predicted_prices - actual_prices)/np.abs(actual_prices)) # test_data['Close']
+print('\nMAPE: %.3f' % float(mape), ', MAPE SCORE: %.2f' % ((mape)*100))
+print('\nFORECASTING ACCURACY: %.2f' % ((1.00 - mape)*100), '% expected accurate in Forecasting the Test Set observations.\n' )
+
+# calculate accuracy
+y_test_dummies = pd.get_dummies(y_test).values
+score = model.evaluate(x_test, y_test_dummies, batch_size=64)
+LSTM_accuracy = score*100
+print('LSTM MODEL PREDICTED Accuracy SCORE: %.2f' % (score*100), '(%)')
+
+# calculate root mean squared error
+testY = scaler.inverse_transform([y_test])
+testScore = math.sqrt(mean_squared_error(testY[0], predicted_prices[:,0]))
+print('LSTM MODEL PREDICTED RMSE: %.2f\n' % (mape*10))
+
+
+
+# Prediction Process Plotting
+plt.figure(figsize=(12,5), dpi=100)
+plt.plot(total_dataset, color = 'orange', label = f"Training {company} Close Price DATA")
+plt.plot(test_data['Close'], color = 'green', label = f"Predicted {company} Close Price DATA")
+plt.title(f"{company} Prediction Process")
+plt.xlabel('Time')
+plt.ylabel(f"{company} Close Price")
+plt.legend(loc='upper left', fontsize=8)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,173 @@
+import datetime
+import numpy as np
+from numpy import array
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+import pandas_datareader.data as web
+
+from math import log
+from RegscorePy import *
+from pandas_datareader.data import DataReader
+from sklearn.metrics import accuracy_score
+from sklearn import metrics
+from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression as LR
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as QDA
+from sklearn.svm import LinearSVC, SVC
+from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn.tree import DecisionTreeClassifier as DTC
+from sklearn.neighbors import KNeighborsClassifier as KNN
+
+# the USD/TRY index: TRY=X
+
+def create_lagged_series(symbol, start_date, end_date, lags=5):
+    """This creates a pandas DataFrame that stores the percentage returns of the 
+    adjusted closing value of a stock obtained from Yahoo Finance, along with 
+    a number of lagged returns from the prior trading days (lags defaults to 5 days).
+    Trading volume, as well as the Direction from the previous day, are also included."""
+
+    # Obtain stock information from Yahoo Finance(gecikmelerden dolayı 365 gün geriden başladık)
+    ts = web.DataReader(symbol, 'yahoo', start_date-datetime.timedelta(days=365), end_date)
+    print("ts: ",ts.shape)
+    print(ts.head())
+    print(ts.tail())
+    
+    # Create the new lagged DataFrame
+    tslag = pd.DataFrame(index=ts.index)
+    tslag["Today"] = ts["Adj Close"]
+    tslag["Volume"] = ts["High"] - ts["Low"] 
+    
+    print("tslag: ",tslag.shape)
+    print(tslag.head())
+    print(tslag.tail())
+    
+    plot_cols = ['Today', 'Volume']
+    plot_features = tslag[plot_cols]
+    plot_features.index = tslag.index
+    _ = plot_features.plot(subplots=True)
+    plot_features = tslag[plot_cols][2904:]    
+    plot_features.index = tslag.index[2904:]
+    _ = plot_features.plot(subplots=True)
+    plt.show()
+
+    # Create the shifted lag series of prior trading period close values
+    for i in range(0,lags):
+        tslag["Lag%s" % str(i+1)] = ts["Adj Close"].shift(i+1)
+
+    # Create the returns DataFrame
+    tsret = pd.DataFrame(index=tslag.index)
+    tsret["Volume"] = tslag["Volume"]
+    tsret["Today"] = tslag["Today"].pct_change()*100.0
+    print("tsret: ",tsret.shape)
+    print(tsret.head())
+    print(tsret.tail())
+    # If any of the values of percentage returns equal zero, set them to
+    # a small number (stops issues with QDA model in scikit-learn)
+    for i,x in enumerate(tsret["Today"]):
+        if (abs(x) < 0.0001):
+            tsret["Today"][i] = 0.0001
+
+    # Create the lagged percentage returns columns
+    for i in range(0,lags):
+        tsret["Lag%s" % str(i+1)] = tslag["Lag%s" % str(i+1)].pct_change()*100.0
+
+    # Create the "Direction" column (+1 or -1) indicating an up/down day
+    tsret["Direction"] = np.sign(tsret["Today"])
+    tsret = tsret[tsret.index >= start_date]
+
+    return tsret
+
+def fit_model(name, model, X_train, y_train, X_test, pred):
+    """Fits a classification model (for our purposes this is LR, LDA and QDA)
+    using the training data, then makes a prediction and subsequent "hit rate"
+    for the test data."""
+
+    
+    # Fit and predict the model on the training, and then test, data
+    model = model.fit(X_train, y_train)
+    pred[name] = model.predict(X_test)
+
+    # Create a series with 1 being correct direction, 0 being wrong
+    # and then calculate the hit rate based on the actual direction
+    pred["%s_Correct" % name] = (1.0+pred[name]*pred["Actual"])/2.0
+    hit_rate = np.mean(pred["%s_Correct" % name])
+    print("----------------------------------------------------------------")
+    print ("The Hit Rate of %s: %.3f" % (name, hit_rate))
+
+    # Calculate the accuracy
+    print("The Accuracy Score %s:  %0.3f" % (name, (accuracy_score(y_test, pred["%s_Correct" % name], normalize=True)*100)))   
+    # hit rate için score hesaplaması yapıldı
+    print("The Hit Score of %s: %0.3f" % (name, model.score(X_test, y_test)*100))
+    #target_names = ['class -1', 'class 0', 'class 1']
+    # classification report 
+    print("The Classification Report of %s: \n" % name)
+    print(classification_report(y_test, (pred["%s_Correct" % name]), target_names=None, zero_division = 1, labels=np.unique(model.predict(X_test))))
+
+    print('%s Mean Absolute Error: %0.3f' % (name, metrics.mean_absolute_error(y_test, pred["%s_Correct" % name])))
+    print('%s Mean Squared Error: %0.3f' % (name, metrics.mean_squared_error(y_test, pred["%s_Correct" % name])))
+    print('%s Root Mean Squared Error: %0.3f' % (name, np.sqrt(metrics.mean_squared_error(y_test, pred["%s_Correct" % name]))))
+    # Akaike’s Information Criterion (En düşük AIC modeli seçilir)
+    print("The AIC of %s: %0.3f" % (name, aic.aic(y_test, (pred["%s_Correct" % name]), len(model.predict(X_test)))))
+    # Bayesian Information Criterion (En düşük BIC modeli seçilir)
+    print("The BIC of %s: %0.3f\n" % (name, bic.bic(y_test, (pred["%s_Correct" % name]), len(model.predict(X_test)))))
+    
+
+if __name__ == '__main__' :
+    # Create a lagged series of the USD/TRY index: TRY=X; XU100.IS = BIST100; ^XU100;  (2021,6,23)
+    snpret = create_lagged_series("TRY=X", datetime.datetime(2016,1,1), datetime.datetime.now(), lags=5)
+    print("snpret: ",snpret.shape)
+    print(snpret.head())
+    print(snpret.tail())
+
+    
+    # Use the prior two days of returns as predictor values, with direction as the response
+    X = snpret[["Lag1","Lag2"]]
+    y = snpret["Direction"]
+
+    # The test data is split into two parts: Before and after 1st Jan 2020.
+    start_test = datetime.datetime(2020,6,23)
+
+    #cv = KFold(n_splits=10, random_state=42, shuffle=False)
+
+    # Create training and test sets
+    X_train = X[X.index < start_test]
+    X_test = X[X.index >= start_test]
+    y_train = y[y.index < start_test]
+    y_test = y[y.index >= start_test]
+
+    # Create prediction DataFrame
+    pred = pd.DataFrame(index=y_test.index)
+    pred["Actual"] = y_test
+
+       
+    # Create and fit the three models    
+    print ("Hit Rates of USD/TRY:")
+    models = [("LR", LR(max_iter=1000000, solver = 'sag', C= 1000000)), ("LDA", LDA(solver = 'lsqr')), ("QDA", QDA()), 
+                        ("LSVC", LinearSVC(max_iter=1000000, C=10)),
+                        ("DTC", DTC(random_state=20)),
+                        #  ("KCM", KerasClassifier(build_fn=baseline_model,
+                        #      epochs=3, batch_size=10,
+                        #      verbose=False)),
+                        ("SVC", SVC(
+                                C=1000000.0, cache_size=200, class_weight=None,
+                                coef0=0.0, degree=3, gamma=0.0001, kernel='rbf',
+                                max_iter=-1, probability=False, random_state=None,
+                                shrinking=True, tol=0.001, verbose=False)),
+                        ("RFC", RFC(
+                                n_estimators=1000, criterion='gini',
+                                max_depth=None, min_samples_split=2,
+                                min_samples_leaf=30, max_features='auto',
+                                bootstrap=True, oob_score=False, n_jobs=1,
+                                random_state=None, verbose=0)
+                            ),
+                        ("KNN", KNN(n_neighbors=5))    
+                            
+                            ]
+    for m in models:
+        fit_model(m[0], m[1], X_train, y_train, X_test, pred)
+        
+     
+    
+ 
